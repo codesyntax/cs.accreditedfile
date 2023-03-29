f66ed8802fca907a63c56f0784f902d0926ed463
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-03-29 16:56:14 +0200

    disable CSRF

--- cs/accreditedfile/browser/accreditation.py
+++ cs/accreditedfile/browser/accreditation.py
@@ -2,13 +2,13 @@ from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
 
 from cs.accreditedfile.subscriber import getPublicationAccreditation
+from plone.protect.interfaces import IDisableCSRFProtection
+from zope.interface import alsoProvides
+
 
 class Accreditation(BrowserView):
     def __call__(self):
+        alsoProvides(self.request, IDisableCSRFProtection)
         context = aq_inner(self.context)
         getPublicationAccreditation(context)
-        return self.request.response.redirect(context.absolute_url() + '/view')
-
-
-        
-    
+        return self.request.response.redirect(context.absolute_url() + "/view")
